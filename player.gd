6be3d65966de7414c5ad75a8f24627ad6b8b2819
arrowira <41136800+arrowira@users.noreply.github.com>
2024-12-14 23:36:01 -0800

    jitterFix

--- player.gd
+++ player.gd
@@ -3,32 +3,36 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-var sensitivity = 0.3;
-signal cbPos;
-signal cbRot;
+var sensitivity = 0.3
 
 @onready var cam_origin: Node3D = $camOriginLeftRight/camOrigin
-
+@onready var lrCam: Node3D = $camOriginLeftRight
 func _input(event):
 	if event is InputEventMouseMotion:
 		#rotate cam left and right
-		rotate_y(deg_to_rad(-event.relative.x * sensitivity))
+		lrCam.rotate_y(deg_to_rad(-event.relative.x * sensitivity))
 		#rotate cam up and down
 		cam_origin.rotate_x(deg_to_rad(-event.relative.y * sensitivity))
 		cam_origin.rotation.x = clamp(cam_origin.rotation.x,deg_to_rad(-90), deg_to_rad(45))
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+var transY = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+	
+
 
 func _physics_process(delta: float) -> void:
-	cbPos.emit(position)
+	
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if Input.is_action_pressed("forward"):
-		cbRot.emit(rotation.y)
+		$MeshInstance3D.rotation.y = lrCam.rotation.y
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -36,7 +40,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (lrCam.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
--- playerMesh.gd
+++ playerMesh.gd
@@ -1,20 +1,23 @@
 extends MeshInstance3D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+	
 
 
 func _on_character_body_3d_cb_pos(val) -> void:
-	position = val
+	#position = val
+	pass
 	
 
 
 func _on_character_body_3d_cb_rot(val) -> void:
-	rotation.y = val
+	#rotation.y = val
+	pass
